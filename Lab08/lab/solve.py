a07ec778e436ce6e86e5ca6477ade0c268aac7ff
eric <ericlin66.cs11@nycu.edu.tw>
2023-05-17 01:13:55 +0800

    Lab08 finished

--- Lab08/lab/solve.py
+++ Lab08/lab/solve.py
@@ -8,14 +8,6 @@ main_addr = 0x4011a9
 find_addr = 0x401371
 avoid_addr = 0x40134d
 
-def handle_scanf_real_input(raw_input):
-    idx = 0
-    for c in raw_input:
-        if c == ord('\n') or c == ord('\0'):
-            break
-        idx += 1
-    return raw_input[:idx]
-
 class MyScanf(angr.SimProcedure):
     def run(self, fmt, n):
         simfd = self.state.posix.get_fd(sys.stdin.fileno())
@@ -27,7 +19,6 @@ proj.hook_symbol('__isoc99_scanf', MyScanf(),  replace=True)
 
 state = proj.factory.blank_state(addr=main_addr)
 
-
 simgr = proj.factory.simulation_manager(state)
 simgr.explore(find=find_addr, avoid=avoid_addr)
 if simgr.found:
@@ -36,7 +27,9 @@ if simgr.found:
 s = simgr.found[0].posix.dumps(sys.stdin.fileno())
 ans = []
 for i in range(15):
-    ans.append(int.from_bytes(s[i * 4 : i * 4 + 4], byteorder='little'))
+    a = int.from_bytes(s[i * 4 : i * 4 + 4], byteorder='little')
+    ans.append(a)
+    print(f"x{i}: {a}")
 
 with open('solve_input', 'w') as f:
     for a in ans:
